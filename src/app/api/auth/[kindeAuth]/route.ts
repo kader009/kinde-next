8b75f3e8a403d881986bc078c2c599348aaa5a90
kader009 <kadermollarasel000@gmail.com>
2024-08-10 12:08:20 +0600

    authorize done

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,9 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextRequest } from 'next/server';
-export async function GET(request: NextRequest, { params }: any) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { kindeAuth: string } }
+) {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import {
+  RegisterLink,
+  LoginLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Register YourSelf!</h1>
+    <div className="flex justify-center items-center min-h-screen">
+      <div>
+        <h1 className='capitalize'>Register YourSelf with kinde</h1>
+        <br />
+
+        <LoginLink className='bg-black text-white p-2 rounded ms-2'>Sign in</LoginLink>
+
+        <RegisterLink className='bg-black text-white p-2 rounded ms-2'>Sign up</RegisterLink>
+      </div>
     </div>
   );
 };
