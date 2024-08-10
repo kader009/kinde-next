47522a41396fcbf8161685588817d6cb024c6d2f
kader009 <kadermollarasel000@gmail.com>
2024-08-10 14:47:54 +0600

    response change

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,9 +1,16 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextRequest } from 'next/server';
+
 export async function GET(
   request: NextRequest,
   { params }: { params: { kindeAuth: string } }
 ) {
   const endpoint = params.kindeAuth;
-  return handleAuth(request, endpoint);
+  const response = await handleAuth(request, endpoint);
+
+  if (response instanceof Response) {
+    return response;
+  }
+
+  return new Response("Unexpected response type", { status: 500 });
 }
